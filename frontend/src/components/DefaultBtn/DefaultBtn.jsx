72fef8f58d69437851cf8d032614bcbc4c07ad9f
Arina <ari.kash117@gmail.com>
2026-01-09 14:02:25 +0300

    finished login page

--- frontend/src/components/DefaultBtn/DefaultBtn.jsx
+++ frontend/src/components/DefaultBtn/DefaultBtn.jsx
@@ -1,9 +1,9 @@
 import styles from './DefaultBtn.module.css';
 
-export default function DefaultBtn({ text, onClick }) {
+export default function DefaultBtn({ text, onClick, className }) {
     return (
         <button 
-            className={styles.btn}
+            className={`${styles.btn} ${className || ''}`}
             onClick={onClick}
         >
             {text}
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,14 +1,32 @@
 import styles from './Login.module.css';
+import DefaultBtn from '../../components/DefaultBtn/DefaultBtn';
 
 export default function Login() {
     return (
         <>
         <div className={styles.form}>
             <span className={styles.text}>Вход</span>
+
             <div className={styles.inputGroup}>
                 <input type="text" placeholder='Логин/email' />
                 <input type="password" placeholder='Пароль' />
             </div>
+
+            <DefaultBtn 
+                text={'Войти'} 
+                className={styles.loginBtn} 
+            />
+
+            <p className={styles.footerText}>
+                Нет аккаунта?{' '}
+                <span
+                    className={styles.link}
+                    // onClick={() => navigate('/register')}
+                >
+                    Зарегистрироваться
+                </span>
+            </p>
+
         </div>
         </>
     );
