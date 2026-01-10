a0185e9c94f5833ae5846ffe998025cdabe552b3
Arina <ari.kash117@gmail.com>
2026-01-10 19:07:06 +0300

    ArtView page ui done

--- frontend/src/pages/ArtView/ArtView.jsx
+++ frontend/src/pages/ArtView/ArtView.jsx
@@ -1,8 +1,57 @@
+import { useState, useEffect, useRef } from 'react';
+
 import styles from './ArtView.module.css';
+
+import UpIcon from '../../assets/up-icon.svg'
 import ArtPost from '../../components/ArtPost/ArtPost';
 
 export default function ArtView() {
+
+    const [showUpBtn, setShowUpBtn] = useState(false);
+    const artPostRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (!artPostRef.current) return;
+
+            const artPostTop = artPostRef.current.offsetTop;
+            const scrollPosition = window.scrollY;
+
+            if (scrollPosition > artPostTop) {
+                setShowUpBtn(true);
+            } else {
+                setShowUpBtn(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
     return (
-        <ArtPost />
+        <div className={styles.content}>
+            <div ref={artPostRef}>
+                <ArtPost />
+            </div>
+
+
+            {showUpBtn && (
+                <button 
+                    className={styles.upBtn}
+                    onClick={scrollToTop}
+                    aria-label="Наверх"
+                >
+                    <img src={UpIcon} alt="up-icon" className={styles.icon}/>
+                </button>
+            )}
+
+            <div className={styles.comments}>
+                {/* <Comment /> */}
+            </div>
+        </div>
     )
 }
