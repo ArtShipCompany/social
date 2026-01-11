c3a16ea59cf827ef0d7c84ea1309f7acb1c14533
Arina <ari.kash117@gmail.com>
2026-01-11 20:15:40 +0300

    added mock data

---
+++ frontend/src/mock-images/mockArts.js
@@ -0,0 +1,63 @@
+import img1 from './джейхины.jpg';
+import img2 from './клоризли.jpg';
+import img3 from './софтикиэимики.jpg';
+import img4 from './biliie.jpg';
+import img5 from './pfp.jpg';
+import img6 from './wenclair.jpg';
+
+const mockUsers = {
+  'user1': { id: 'user1', nickname: 'crow_666_wa', pfp: img5 },
+  'user2': { id: 'user2', nickname: 'biliie_art', pfp: img4 },
+  'user3': { id: 'user3', nickname: 'sofka_miki', pfp: img3 },
+  'user4': { id: 'user4', nickname: 'wenclair', pfp: img6 },
+  'user5': { id: 'user5', nickname: 'jeyhiny', pfp: img1 },
+  'user6': { id: 'user6', nickname: 'klorizli', pfp: img2 },
+};
+
+export const mockArts = [
+  { 
+    id: '1', 
+    image: img1, 
+    description: 'Джейхины', 
+    tags: '#duo #anime',
+    ownerId: 'user5'
+  },
+  { 
+    id: '2', 
+    image: img2, 
+    description: 'Клоризли', 
+    tags: '#solo #mood',
+    ownerId: 'user6'
+  },
+  { 
+    id: '3', 
+    image: img3, 
+    description: 'Софтика и Эймика', 
+    tags: '#cute #pastel',
+    ownerId: 'user3'
+  },
+  { 
+    id: '4', 
+    image: img4, 
+    description: 'Biliie', 
+    tags: '#portrait #vibe',
+    ownerId: 'user2'
+  },
+  { 
+    id: '5', 
+    image: img5, 
+    description: 'PFP', 
+    tags: '#avatar #simple',
+    ownerId: 'user1'
+  },
+  { 
+    id: '6', 
+    image: img6, 
+    description: 'Wenclair', 
+    tags: '#wenclair #aesthetic',
+    ownerId: 'user4'
+  },
+];
+
+export const mockArtsMap = Object.fromEntries(mockArts.map(art => [art.id, art]));
+export const mockUsersMap = mockUsers;
