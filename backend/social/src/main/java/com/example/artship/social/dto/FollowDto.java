468bc548dc4efb41db3f7080c53ecc225ba4012b
westsunshine <aisaxytsaeva@mail.ru>
2026-04-05 16:07:14 +0300

    Fixed in follow logic

--- backend/social/src/main/java/com/example/artship/social/dto/FollowDto.java
+++ backend/social/src/main/java/com/example/artship/social/dto/FollowDto.java
@@ -4,35 +4,39 @@ import com.example.artship.social.model.Follow;
 import java.time.LocalDateTime;
 
 public class FollowDto {
-    private Long followerId;
-    private Long followingId;
-    private String followerUsername;
-    private String followingUsername;
+    private UserFollowInfoDto follower;
+    private UserFollowInfoDto following;
     private LocalDateTime createdAt;
     
     public FollowDto() {}
     
     public FollowDto(Follow follow) {
-        this.followerId = follow.getFollower().getId();
-        this.followingId = follow.getFollowing().getId();
-        this.followerUsername = follow.getFollower().getUsername();
-        this.followingUsername = follow.getFollowing().getUsername();
+        this.follower = new UserFollowInfoDto(follow.getFollower());
+        this.following = new UserFollowInfoDto(follow.getFollowing());
         this.createdAt = follow.getCreatedAt();
     }
     
-
-    public Long getFollowerId() { return followerId; }
-    public void setFollowerId(Long followerId) { this.followerId = followerId; }
+    public UserFollowInfoDto getFollower() { 
+        return follower; 
+    }
     
-    public Long getFollowingId() { return followingId; }
-    public void setFollowingId(Long followingId) { this.followingId = followingId; }
+    public void setFollower(UserFollowInfoDto follower) { 
+        this.follower = follower; 
+    }
     
-    public String getFollowerUsername() { return followerUsername; }
-    public void setFollowerUsername(String followerUsername) { this.followerUsername = followerUsername; }
+    public UserFollowInfoDto getFollowing() { 
+        return following; 
+    }
     
-    public String getFollowingUsername() { return followingUsername; }
-    public void setFollowingUsername(String followingUsername) { this.followingUsername = followingUsername; }
+    public void setFollowing(UserFollowInfoDto following) { 
+        this.following = following; 
+    }
     
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public LocalDateTime getCreatedAt() { 
+        return createdAt; 
+    }
+    
+    public void setCreatedAt(LocalDateTime createdAt) { 
+        this.createdAt = createdAt; 
+    }
 }
